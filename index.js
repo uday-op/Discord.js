c81d981d4f54135a161425ed5099849f666d9a42
uday-op <79244165+uday-op@users.noreply.github.com>
2021-03-10 21:33:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const config = require('./config.json');
 
 client.on('ready', () => {
     console.log("Bot Is Ready!")
-    //CHANGE {type: 2} in 
-    //1 FOR PLAYING
-    //2 FOR LISTENING
-    //3 FOR WATCHING
     client.user.setActivity("MADE BY UDAY!", {type: 3});
   });
 
